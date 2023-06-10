e2781e2ba6d26a343004e55ffb951818da2e7e4b
Caio Sella <sellacaio4@gmail.com>
2023-06-10 00:19:01 -0300

    Versão finalizada

---
+++ Trabalho_II.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Trabalho_II{
+	static void printPhraseDiscoded(int countTamDiscoded, String phraseCoded){
+		int finalIntPhrase;
+		char finalCharPhrase;
+		
+		System.out.print("Palavra descodada: ");
+		
+		for(int i  = 0; i < phraseCoded.length(); i++){
+			if(phraseCoded.charAt(i) == 32){
+				System.out.print(" ");
+				continue;
+			}
+				
+			finalIntPhrase = phraseCoded.charAt(i) - countTamDiscoded;
+			finalCharPhrase =  (char)finalIntPhrase;
+			
+			System.out.printf("%s", finalCharPhrase);
+		}
+	}
+	
+	public static void main(String[] args){
+		String phraseCoded, phraseDiscoded;
+		int numberAsciiCoded, numberAsciiDiscoded = 0, countTamValue = 0, countValueCoded = 0;
+		int aux = 0, i = 0;
+		Scanner readPhrases = new Scanner(System.in);
+		
+		System.out.print("Digite a frase codificada: ");
+		phraseCoded = readPhrases.nextLine();
+		System.out.print("Digite a frase para decodificar: ");
+		phraseDiscoded = readPhrases.nextLine();
+
+		for(int a = 0; a < 26; a++){	
+			for(int j = aux; j < phraseCoded.length(); j++){
+				if(countTamValue == phraseDiscoded.length()){
+					System.out.printf("Achei a palavra, numero de posicoes: %d\n", countValueCoded);
+					printPhraseDiscoded(countValueCoded, phraseCoded);
+					return;
+				}
+				
+				numberAsciiCoded = phraseCoded.charAt(j);
+				
+				numberAsciiDiscoded = phraseDiscoded.charAt(i) + countValueCoded;
+				
+				if(numberAsciiCoded == numberAsciiDiscoded){
+					countTamValue++;
+					i++;	
+					continue;
+				}		
+				countTamValue = 0;
+				i = 0;
+			}
+			countValueCoded++;
+			aux = 0;
+		}
+		System.out.print("Não achei :(");
+	}
+}
